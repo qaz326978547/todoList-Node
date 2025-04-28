174a9975bc7dd478f2b6f399dc492238339aaffb
Bean <qaz326978547@gmail.com>
2025-04-28 21:34:24 +0800

    no message

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -3,11 +3,11 @@ import { Todo } from '../entity/todo.entity'
 import { User } from '../entity/user.entity'
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     entities: [Todo, User],
+    synchronize: true,
 })
